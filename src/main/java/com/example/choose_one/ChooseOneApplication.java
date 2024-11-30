8cf625586eff075262c4b91b68598f2df17b7dca
ibtsdan <ibtsdan@gmail.com>
2024-11-30 22:05:55 +0900

    init

---
+++ src/main/java/com/example/choose_one/ChooseOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.choose_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseOneApplication.class, args);
+	}
+
+}
