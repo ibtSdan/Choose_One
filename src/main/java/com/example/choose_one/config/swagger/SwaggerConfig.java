b75fbb2fc3bf15e01fa5cffce45c0d81c673bd6d
ibtsdan <ibtsdan@gmail.com>
2024-12-04 23:11:57 +0900

    ObjectMapper 설정

---
+++ src/main/java/com/example/choose_one/config/swagger/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.example.choose_one.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class SwaggerConfig {
+    @Bean
+
+}
