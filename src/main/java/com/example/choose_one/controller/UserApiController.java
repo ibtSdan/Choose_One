0b0e495e61be67036bc5cd10b4412523d7f28245
ibtsdan <ibtsdan@gmail.com>
2024-12-04 23:57:09 +0900

    Swagger 설정

--- src/main/java/com/example/choose_one/controller/UserApiController.java
+++ src/main/java/com/example/choose_one/controller/UserApiController.java
@@ -5,6 +5,7 @@ import com.example.choose_one.model.LoginRequest;
 import com.example.choose_one.model.LoginResponse;
 import com.example.choose_one.model.SignUpRequest;
 import com.example.choose_one.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,12 +24,14 @@ public class UserApiController {
 
     // 회원가입
     @PostMapping("/signup")
+    @Operation(summary = "User 회원가입",description = "User 회원가입 할 때 사용하는 API")
     public Api<String> signUp(@Valid @RequestBody SignUpRequest signUpRequest){
         return userService.signUp(signUpRequest);
     }
 
     // 로그인
     @PostMapping("/login")
+    @Operation(summary = "User 로그인",description = "User 로그인 할 때 사용하는 API")
     public Api<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest){
         return userService.login(loginRequest);
     }
--- src/main/java/com/example/choose_one/controller/VoteApiController.java
+++ src/main/java/com/example/choose_one/controller/VoteApiController.java
@@ -3,6 +3,9 @@ package com.example.choose_one.controller;
 import com.example.choose_one.common.Api;
 import com.example.choose_one.model.VoteRequest;
 import com.example.choose_one.service.VoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +19,8 @@ public class VoteApiController {
     private final VoteService voteService;
 
     @PostMapping("/create")
+    @Operation(summary = "투표하기",description = "User가 투표할 때 사용하는 API")
+    @Parameter(name = "voteOption",description = "A or B")
     public Api<String> create(@Valid @RequestBody VoteRequest voteRequest){
         return voteService.create(voteRequest);
     }
