c94b8e9c6cfb8e51a09e98abfddf1848c45961c4
ibtsdan <ibtsdan@gmail.com>
2024-12-12 12:25:40 +0900

    feat: security 권한 추가

--- src/main/java/com/example/choose_one/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/choose_one/filter/JwtAuthenticationFilter.java
@@ -8,12 +8,18 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -26,12 +32,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         log.info("필터");
         String token = request.getHeader("authorization");
         try{
-            Long userId = tokenService.validationToken(token);
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId,null,null);
+            var map = tokenService.validationToken(token);
+            var userId = Long.parseLong(map.get("userId").toString());
+            var authorities = (List<String>) map.get("authorities");
+            var granted = authorities.stream()
+                    .map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId,null,granted);
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
         } catch (Exception e){
+            log.info("으악"+e);
             SecurityContextHolder.clearContext();
         }
         filterChain.doFilter(request,response);
