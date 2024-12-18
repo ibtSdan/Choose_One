0e04aa8753e7c1fb33fe54020b087b76bce3bb23
ibtsdan <ibtsdan@gmail.com>
2024-12-18 23:51:27 +0900

    chore: websocket test 페이지 생성

--- src/main/java/com/example/choose_one/config/security/SecurityConfig.java
+++ src/main/java/com/example/choose_one/config/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                                 .requestMatchers("/swagger-ui.index.html",
                                         "/swagger-ui/**",
                                         "/v3/api-docs/**",
+                                        "/", // websocket 테스트 용
                                         "/user/signup",
                                         "/user/login",
                                         "/token/reissue").permitAll()
