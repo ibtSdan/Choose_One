f6e652329d7ec103833b8f017a4e6eaefbca2a74
ibtsdan <ibtsdan@gmail.com>
2025-04-10 14:34:04 +0900

    feat: post 비관적 락 구현

--- src/main/java/com/example/choose_one/repository/PostRepository.java
+++ src/main/java/com/example/choose_one/repository/PostRepository.java
@@ -1,12 +1,21 @@
 package com.example.choose_one.repository;
 
 import com.example.choose_one.entity.PostEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
     Page<PostEntity> findByUserId(Long userId, Pageable pageable);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM post p WHERE p.id = :id")
+    Optional<PostEntity> findByIdForUpdate(@Param("id") Long id);
 }
