19566b2cb2618814c1950dc3a9d66a8f4815c3fd
ibtsdan <ibtsdan@gmail.com>
2025-04-10 14:06:43 +0900

    vote metrics & test 환경 구성

--- src/main/java/com/example/choose_one/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/choose_one/filter/JwtAuthenticationFilter.java
@@ -39,7 +39,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             "/actuator/**",
             "/user/signup",
             "/user/login",
-            "/token/reissue"
+            "/token/reissue",
+            "/vote/create"
     ));
 
     @Override
