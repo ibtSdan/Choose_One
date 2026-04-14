46d0a0c1de17493da8cc5d60a81ca4bbd3c3c98a
ibtSdan <ibtsdan@gmail.com>
2026-04-14 22:10:39 +0900

    refactor: redisson 적용

--- src/main/java/com/example/choose_one/service/VoteService.java
+++ src/main/java/com/example/choose_one/service/VoteService.java
@@ -38,10 +38,10 @@ public class VoteService {
                 .orElseThrow(()-> new ApiException(UserErrorCode.USER_NOT_FOUND));
 
         // 락 X
-        /*
+
         var post = postRepository.findById(voteRequest.getPostId())
                 .orElseThrow(() -> new ApiException(PostErrorCode.POST_NOT_FOUND, "올바른 post id를 입력하십시오."));
-         */
+
 
         // 비관적 락
         /*
@@ -50,9 +50,10 @@ public class VoteService {
          */
 
         // 낙관적 락
+        /*
         var post = postRepository.findByIdOptimistic(voteRequest.getPostId())
                 .orElseThrow(() -> new ApiException(PostErrorCode.POST_NOT_FOUND, "올바른 post id를 입력하십시오."));
-
+         */
 
         if(!(voteRequest.getVoteOption()=='A' || voteRequest.getVoteOption()=='B')){
             throw new ApiException(VoteErrorCode.INVALID_VOTE,"A or B 중 선택 가능합니다.");
@@ -69,7 +70,7 @@ public class VoteService {
                 .post(post)
                 .voteOption(voteRequest.getVoteOption())
                 .build();
-        //voteRepository.save(entity);
+        voteRepository.save(entity);
 
         if (voteRequest.getVoteOption() == 'A') {
             post.setVoteCountA(post.getVoteCountA() + 1);
